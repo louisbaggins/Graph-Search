caae6c9e8cff8f9349750c0d7b5697e1d3d9fe43
Luis Felipe <luisaraujo@mac-mini-de-luis.local>
2019-06-10 22:10:15 -0300

    Initial Commit

---
+++ GraphTp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GraphTp
+//
+//  Created by Luis Araújo on 10/06/2019.
+//  Copyright © 2019 UFMG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
