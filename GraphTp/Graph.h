a95b82938db672341840f09d14b108b51559d82b
Luis Araújo <luisaraujo@Mac-mini-de-Luis.local>
2019-06-16 08:52:03 -0300

    Complete the graph and verify if the graph is complete method

--- GraphTp/Graph.h
+++ GraphTp/Graph.h
@@ -6,6 +6,7 @@
 //  Copyright © 2019 UFMG. All rights reserved.
 //
 #include <iostream>
+#include <list>
 #include <stdlib.h>
 #ifndef Graph_h
 #define Graph_h
@@ -15,6 +16,18 @@ class Graph
 private:
     int n_Vertices;
     int n_Arestas;
+    
+    void SetNumberOfArchs()
+    {
+        for(int a = 0; a<this->n_Vertices; a++)
+        {
+            for(int b = 0; b<this->n_Vertices; b++)
+            {
+                if(this->Matriz[a][b])
+                    this->n_Arestas++;
+            }
+        }
+    }
 public:
     int **Matriz;
     
@@ -32,7 +45,20 @@ public:
             for(int b =0; b<this->n_Vertices; b++){
                 Matriz[a][b] = (rand() % 2 - 0);
             }
+        }        
+        this->SetNumberOfArchs();
+    }
+    
+    Graph(int *matrice[5])
+    {
+        this->n_Vertices = 5;
+        this->Matriz = (int **)calloc(n_Vertices, sizeof(int*));
+        for(int a = 0; a<this->n_Vertices; a++)
+        {
+            this->Matriz[a] = new int[n_Vertices];
         }
+        this->Matriz = matrice;
+        this->SetNumberOfArchs();
     }
     //insert a arc by coordenates
     bool Insert(int x_coordenates, int y_coordenates)
@@ -69,6 +95,17 @@ public:
             throw e;
         }
     }
+    
+    //return numbers of arch and vertices in graph
+    list<int> ReturnNumberOfArchAndVertices()
+    {
+        list<int> *archsAndVertices = new list<int>;
+        archsAndVertices->push_back(this->n_Vertices);
+        archsAndVertices->push_back(this->n_Arestas);
+        
+        return *archsAndVertices;
+    }
+    
     //verify if there's a arc betwen the two vertices
     bool Edge(int x_coordenates, int y_coordenates)
     {
@@ -85,6 +122,29 @@ public:
             throw e;
         }
     }
+    
+    //verify if the graph is complete
+    bool VerifyIfTheGraphIsComplete()
+    {
+        int numberOfPositions = n_Vertices*n_Vertices;
+        if(this->n_Arestas == numberOfPositions)
+            return true;
+        return false;
+    }
+    //Completes the graph
+    void CompletesTheGraph()
+    {
+        for(int a = 0; a<this->n_Vertices; a++){
+            for(int b = 0; b<this->n_Vertices; b++){
+                if(this->Matriz[a][b])
+                    this->Matriz[a][b] = 1;
+            }
+        }
+        
+    }
+    
+    
+    
 };
 
 #endif /* Graph_h */
--- GraphTp/main.cpp
+++ GraphTp/main.cpp
@@ -18,9 +18,35 @@ void PrintMatrice(int **matrice)
         cout<<endl;
     }
 }
+
+int** InitializeMatrice(int **matrice, int i)
+{
+    for(int a = 0; a< 5; a++){
+        for(int b = 0; b<5; b++){
+           matrice[a][b] = i;
+        }
+    }
+    return matrice;
+}
+int** InitializeMatricePointer(int **matrice, int n)
+{
+    matrice = (int **)calloc(n, sizeof(int*));
+    for(int a = 0; a< 5; a++){
+            matrice[a] = new int[n];
+    }
+    return InitializeMatrice(matrice, n);
+}
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     static Graph *graph = new Graph(5);
+    static int **matrix;
+    matrix = InitializeMatricePointer(matrix, 5);
+    Graph *matrice = new Graph(matrix);
+    //InitializeMatrice(matrix, 1);
+
+    
     PrintMatrice(graph->Matriz);
     
     
@@ -30,6 +56,18 @@ int main(int argc, const char * argv[]) {
     cout<< graph->Remove(1,0)<<endl;
     PrintMatrice(graph->Matriz);
     cout<< graph->Remove(2,0)<<endl;
+    for(auto g : graph->ReturnNumberOfArchAndVertices()){
+        cout<< g << endl;
+    }
+    cout<<graph->VerifyIfTheGraphIsComplete()<<endl;
+    cout<<matrice->VerifyIfTheGraphIsComplete()<<endl;
+    PrintMatrice(matrice->Matriz);
+    
+    for(auto g : matrice->ReturnNumberOfArchAndVertices()){
+        cout<< g << endl;
+    }
+    
+    
     cout << "Hello, World!\n";
     return 0;
 }
